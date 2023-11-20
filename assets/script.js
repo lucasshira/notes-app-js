117fb451a282bc90c90aa73e304c9f6165457848
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2023-11-20 16:51:53 -0300

    second commit

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,16 @@ const notesEl = document.querySelector('.notes');
 const editBtn = document.querySelector('.edit');
 const deleteBtn = document.querySelector('.delete');
 
-const main = document.querySelector('.main');
-const textArea = document.querySelector('textarea');
+const main = notesEl.querySelector('.main');
+const textArea = notesEl.querySelector('textarea');
 
 editBtn.addEventListener('click', () => {
     main.classList.toggle('hidden');
     textArea.classList.toggle('hidden');
+});
+
+textArea.addEventListener('input', (e) => {
+    const { value } = e.target;
 
     main.innerHTML = marked(value);
 });
